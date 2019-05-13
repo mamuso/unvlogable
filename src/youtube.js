79ca3dce981919d5ccedecbd5c42c82fd06f7a4e
Manuel Muñoz Solera <mamuso@mamuso.net>
2019-05-13 12:03:19 -0700

    Basic scaffold to retrieve data from an oembed endpoint

--- src/youtube.js
+++ src/youtube.js
@@ -1,5 +1,21 @@
-const youtube = (videourl, options) => {
-  return `youtube`;
+const helpers = require("./helpers.js");
+
+const youtube = async (videourl, options) => {
+  // youtube oembed, returns a json
+  const url = `https://www.youtube.com/oembed?url=${videourl}&format=json`;
+
+  // gettign the data
+  response = await helpers.getData(url);
+  let { title, thumbnail_url, html, width, height } = response;
+
+  // making the thumbnail better if we can
+  thumbnail_url = thumbnail_url.replace("hqdefault", "maxresdefault");
+
+  return {
+    title: title,
+    thumbnail: thumbnail_url,
+    embed: html
+  };
 };
 
 module.exports = youtube;
--- test/unvlogable.test.js
+++ test/unvlogable.test.js
@@ -1,5 +1,23 @@
 const unvlogable = require("../unvlogable");
 
-test("calling without a url shoud return false", () => {
-  expect(unvlogable()).toBe(false);
+// test urls
+const youtubeurl = "https://www.youtube.com/watch?v=mqOEzEPZ8iw";
+const youtuurl = "https://youtu.be/Sj3Fsgx6NAg";
+
+test("calling without a url shoud return false", async () => {
+  expect.assertions(1);
+  const data = await unvlogable();
+  expect(data).toBe(false);
 });
+
+// Youtube test
+test("calling with a youtube url", async () => {
+  expect.assertions(2);
+  const data = await unvlogable("https://www.youtube.com/watch?v=mqOEzEPZ8iw");
+  expect(data).not.toBe(null);
+  expect(data.title).toBe("OP-1 07-01-18 (Magic)");
+});
+
+// test("calling without a youtu.be url", () => {
+//   expect(unvlogable(youtuurl)).toBe(false);
+// });
